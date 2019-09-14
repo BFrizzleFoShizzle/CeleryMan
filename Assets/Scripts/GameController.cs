7f71d9e076766788c0c01cc052440366b4aed402
Jack <jackbannistersutton@gmail.com>
2019-09-14 14:06:45 +1200

    Added UI and Gamecontroller

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    public int money; 
+    public int moneypersec = 100;
+    public Text moneytext;
     // Start is called before the first frame update
     void Start()
     {
-        
+        money = 4000;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        money -= (int)(Time.deltaTime*moneypersec);
+        moneytext.text = "Money "+(money)+"$";
     }
 }
