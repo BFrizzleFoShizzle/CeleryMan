25b748f27195d61eed7989ae58f34427a3f9cb2b
Thomas Auberson <thomas.auberson@gmail.com>
2019-09-15 16:37:30 +1200

    Imported Animation

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public Animator anim;
+    public PlayerController pController;
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool("running", pController.moving);
+    }
+}
